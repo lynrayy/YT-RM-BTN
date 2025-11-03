9053939c46c5391f7ca3bf2573faf7db143ca445
lynrayy <59417884+lynrayy@users.noreply.github.com>
2025-11-03 22:55:35 +0300

    Modern icon update

--- Youtube -Remove From Playlist- Button-1.600.user.js
+++ Youtube -Remove From Playlist- Button-1.600.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name      Youtube "Remove From Playlist" Button
 // @namespace http://tampermonkey.net/
-// @version   1.6
+// @version   1.7
 // @description Adds a button next to the three dots menu to remove videos from a playlist with one click on YouTube
 // @author    Lynrayy + art13
 // @match     https://www.youtube.com/*
 // @grant     none
 // @license   MIT
-// @source   https://github.com/lynrayy/YT-RM-BTN
+// @source
 // ==/UserScript==
 
 (function() {
@@ -34,7 +34,7 @@
 
         // Добавляем путь для иконки корзины
         const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-        path.setAttribute('d', 'M11 17H9V8h2v9zm4-9h-2v9h2V8zm4-4v1h-1v16H6V5H5V4h4V3h6v1h4zm-2 1H7v15h10V5z');
+        path.setAttribute('d', 'M19 3h-4V2a1 1 0 00-1-1h-4a1 1 0 00-1 1v1H5a2 2 0 00-2 2h18a2 2 0 00-2-2ZM6 19V7H4v12a4 4 0 004 4h8a4 4 0 004-4V7h-2v12a2 2 0 01-2 2H8a2 2 0 01-2-2Zm4-11a1 1 0 00-1 1v8a1 1 0 102 0V9a1 1 0 00-1-1Zm4 0a1 1 0 00-1 1v8a1 1 0 002 0V9a1 1 0 00-1-1Z');
         trashIcon.appendChild(path);
 
         buttonContainer.appendChild(trashIcon);
